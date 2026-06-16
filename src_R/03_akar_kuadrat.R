3bdf3693b1d4e991899816b294cb958830aecd6f
kelvidici <eikel.gs@mhs.unsoed.ac.id>
2026-06-16 14:39:51 +0700

    Eikel - Menambah Program Soal Nomor 3

--- src_R/03_akar_kuadrat.R
+++ src_R/03_akar_kuadrat.R
@@ -1,46 +1,37 @@
 #Soal 3
 #03_akar_kuadrat.R
 
-# Fungsi untuk menghitung akar persamaan kuadrat
-hitung_akar_kuadrat <- function() {
-  cat("--- Program Hitung Akar Persamaan Kuadrat (ax^2 + bx + c = 0) ---\n")
-  
-  # Membaca input koefisien dari user
-  a <- as.numeric(readline(prompt = "Masukkan koefisien a: "))
-  b <- as.numeric(readline(prompt = "Masukkan koefisien b: "))
-  c <- as.numeric(readline(prompt = "Masukkan koefisien c: "))
-  
-  # Validasi jika a = 0 (bukan persamaan kuadrat)
-  if (a == 0) {
-    cat("Nilai 'a' tidak boleh 0 karena bukan merupakan persamaan kuadrat.\n")
-    return()
-  }
-  
-  # Menghitung Diskriminan (D)
-  D <- (b^2) - (4 * a * c)
-  
-  # Pengecekan kondisi Diskriminan
-  if (D > 0) {
-    # Dua akar real yang berbeda
-    x1 <- (-b + sqrt(D)) / (2 * a)
-    x2 <- (-b - sqrt(D)) / (2 * a)
-    
-    cat("\nPersamaan memiliki dua akar real yang berbeda:\n")
-    cat(sprintf("x1 = %.3f\n", x1))
-    cat(sprintf("x2 = %.3f\n", x2))
-    
-  } else if (D == 0) {
-    # Satu akar real (kembar)
-    x <- -b / (2 * a)
-    
-    cat("\nPersamaan memiliki satu akar real (kembar):\n")
-    cat(sprintf("x = %.3f\n", x))
-    
-  } else {
-    # Akar imajiner jika D < 0
-    cat("\nPersamaan tersebut hanya memiliki akar-akar imajiner.\n")
-  }
-}
+# input nilai
+a <- as.numeric(readline("Masukkan nilai a: "))
+b <- as.numeric(readline("Masukkan nilai b: "))
+c <- as.numeric(readline("Masukkan nilai c: "))
 
-# Menjalankan fungsi program
-hitung_akar_kuadrat()
+    # menghitung diskriminan
+    D <- b^2 - 4*a*c
+    
+    if (D > 0) {
+      
+      x1 <- (-b + sqrt(D)) / (2*a)
+      x2 <- (-b - sqrt(D)) / (2*a)
+      
+      cat("Persamaan memiliki dua akar real\n")
+      cat("x1 =", x1, "\n")
+      cat("x2 =", x2, "\n")
+      
+    } else if (D == 0) {
+      
+      x <- -b / (2*a)
+      
+      cat("Persamaan memiliki satu akar real kembar\n")
+      cat("x =", x, "\n")
+      
+    } else {
+      
+      bagian_real <- -b / (2*a)
+      bagian_imajiner <- sqrt(abs(D)) / (2*a)
+      
+      cat("Persamaan memiliki akar imajiner\n")
+      cat("x1 =", bagian_real, "+", bagian_imajiner, "i\n")
+      cat("x2 =", bagian_real, "-", bagian_imajiner, "i\n")
+      
+    }
