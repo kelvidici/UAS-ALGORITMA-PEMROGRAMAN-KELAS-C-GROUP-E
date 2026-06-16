8d1f7ec0ef2b5795a463fe3621dbab3bda5c89ea
kelvidici <eikel.gs@mhs.unsoed.ac.id>
2026-06-16 14:41:28 +0700

    Eikel - Menambah Program Soal Nomor 1

--- src_R/01_analisis_teks.R
+++ src_R/01_analisis_teks.R
@@ -1,13 +1,28 @@
 #Soal 1
 #01_analisis_teks.R
 
-#input teks
-teks <- "Media sosial atau disebut juga dengan jejaring sosial seperti Facebook, Twitter, Instagram dan masih banyak lagi ternyata tidak hanya digunakan sebagai tempat berkumpul atau berbagi di dunia maya. Namun, media sosial kini juga bisa dimanfaatkan sebagai media untuk mengembangkan sebuah bisnis. Saat ini telah banyak para pengusaha yang beralih ke media sosial dalam memasarkan produk mereka baik barang ataupun jasa. Beralihnya para pelaku bisnis ke media ini dikarenakan jejaring sosial memiliki manfaat yang sangat baik bagi usaha bisnis. Berikut ini adalah alasan mengapa jejaring sosial bisa menjadi alat promosi yang paling efektif."
-#hitung jumlah kalimat
-jumlah_kalimat <- length(unlist(strsplit(teks, "\\.")))
-#hitung jumlah kata
-kata <- unlist(strsplit(teks, " "))
-jumlah_kata<- length(kata)
-#menampilkan hasil
+# fungsi hitung jumlah kalimat
+hitung_kalimat <- function(teks){
+  length(unlist(strsplit(teks, "\\."))) - 1
+}
+
+# fungsi hitung jumlah kata
+hitung_kata <- function(teks){
+  kata <- unlist(strsplit(teks, " "))
+  length(kata)
+}
+
+# input teks
+teks <- "Bahan Bakar Minyak (BBM) merupakan salah satu sumber energi yang paling banyak digunakan dalam kehidupan sehari-hari. BBM berasal dari proses pengolahan minyak bumi yang menghasilkan berbagai jenis bahan bakar, seperti bensin, solar, dan avtur. Penggunaan BBM sangat penting untuk mendukung kegiatan transportasi, industri, pembangkit listrik, serta berbagai aktivitas ekonomi lainnya. Kendaraan bermotor, baik roda dua maupun roda empat, umumnya menggunakan BBM sebagai sumber tenaga utama. Meskipun memiliki peran yang besar dalam menunjang kehidupan modern, penggunaan BBM yang berlebihan dapat menimbulkan berbagai dampak negatif, seperti pencemaran udara dan peningkatan emisi gas rumah kaca yang berkontribusi terhadap perubahan iklim. Oleh karena itu, diperlukan upaya untuk menggunakan BBM secara bijak dan efisien. Selain itu, pengembangan energi alternatif seperti tenaga surya, angin, dan listrik juga perlu terus ditingkatkan guna mengurangi ketergantungan terhadap BBM. Dengan demikian, kebutuhan energi masyarakat dapat tetap terpenuhi sekaligus menjaga kelestarian lingkungan untuk generasi mendatang.
+"
+
+# hitung jumlah kalimat
+jumlah_kalimat <- hitung_kalimat(teks)
+
+# hitung jumlah kata
+jumlah_kata <- hitung_kata(teks)
+
+# menampilkan hasil
 cat("Jumlah kalimat =", jumlah_kalimat, "\n")
 cat("Jumlah kata =", jumlah_kata)
+
